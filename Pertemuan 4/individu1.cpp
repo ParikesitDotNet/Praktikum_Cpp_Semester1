fbd5aa2c16acc1f0a26bc7529cb478fcba9b1921
ParikesitDotNet <yojannakesit@gmail.com>
2026-09-16 17:58:12 +0700

    Test pertama program individu1.cpp

---
+++ Pertemuan 4/individu1.cpp
@@ -0,0 +1,76 @@
+#include "header.h"
+
+// Function Diskon
+float giveDiscount(float persen, float harga)
+{
+    float theDiscountedHarga = harga - (harga * (persen / 100.0));
+    return theDiscountedHarga;
+}
+
+int main()
+{
+    int pilihanUser;
+    bool isTambahBelanjaan = true;
+
+    while (isTambahBelanjaan)
+    {
+        int jumlahBarang = 0;
+        float totalHarga = 0;
+        float outputHarga = 0;
+
+        // Input jumlah barang
+        cout << left;
+        cout << setw(25) << "Masukkan jumlah barang: ";
+        cin >> jumlahBarang;
+        cout << endl;
+
+        float listHargaBarangArr[jumlahBarang];
+
+        // Looping total barang dan input harga masing-masing barang
+        for (int i = 0; i < jumlahBarang; i++)
+        {
+            cout << "Masukkan harga barang ke-" << i + 1 << ": Rp ";
+            cin >> listHargaBarangArr[i];
+            totalHarga += listHargaBarangArr[i];
+        }
+
+        // Desicion output harga
+        if (totalHarga > 500000)
+            outputHarga = giveDiscount(10, totalHarga);
+
+        else if (totalHarga >= 250000 && totalHarga <= 500000)
+            outputHarga = giveDiscount(5, totalHarga);
+
+        else
+            outputHarga = totalHarga;
+
+        cout << endl;
+        cout << setw(25) << "Total Harga" << ": Rp " << totalHarga << endl;
+        cout << setw(25) << "Diskon" << ": Rp " << totalHarga - outputHarga << endl;
+        cout << setw(25) << "Total Setelah Diskon" << ": Rp " << outputHarga << endl;
+        cout << endl;
+
+        do
+        {
+            cout << "Ingin menambahkan belanjaan lagi? (1 untuk ya, 2 untuk tidak): ";
+            cin >> pilihanUser;
+
+            if (pilihanUser == 1)
+            {
+                isTambahBelanjaan = true;
+                cout << "Silahkan masukkan kembali harga belanjaan anda!\n";
+            }
+            else if (pilihanUser == 2)
+            {
+                isTambahBelanjaan = false;
+                cout << "Terimakasih telah menggunakan program ini!\n";
+            }
+            else
+                cout << "Mohon masukkan pilihan yang benar!\n";
+            cout << endl;
+
+        } while (pilihanUser != 1 && pilihanUser != 2);
+    }
+
+    return 0;
+}
